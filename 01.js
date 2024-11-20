b5d9777981a5678dab8b0075b3781f8b2d96df55
Roky <apple@Rokys-MacBook-Air.local>
2024-11-20 20:43:17 +0900

    convert celsiusToFahrenheit

---
+++ 01.js
@@ -0,0 +1,10 @@
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function celsiusToFahrenheit(celsius) {
+    let isFahrenheit = (celsius * 9/5)+32;
+    return isFahrenheit;
+}
+let result = celsiusToFahrenheit(-20);
+console.log(result);
+
+
